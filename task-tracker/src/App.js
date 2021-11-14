1b360804815ea5bbc77cdb830876a908d3ef6828
Sonal Lakhotia <jsssonal@yahoo.com>
2021-11-14 15:33:27 +0100

    Added the tasks and the delete functionality

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,10 +1,36 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [ tasks, setTasks ] = useState([
+    {
+      id: 1,
+      text: 'Meeting with clients',
+      day: 'Feb 5, 2021'
+    },
+    {
+      id: 2,
+      text: 'Grocery Shopping',
+      day: 'Dec 2, 2021'
+    },
+    {
+      id: 3,
+      text: 'Call the House manager',
+      day: 'Dec 12, 2021'
+    }
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id ))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello Universe!</h1>
-      </header>
+    <div className="container">
+      <Header/>
+      {tasks.length > 0 ? 
+        <Tasks tasks={tasks} deleteTask={deleteTask} />
+        : 'No Tasks to Show'}
     </div>
   );
 }
---
+++ task-tracker/src/components/Tasks.js
@@ -0,0 +1,17 @@
+import Task from "./Task"
+
+const Tasks = ({tasks, deleteTask}) => {
+  return(
+    <>
+    {tasks.map((task) => (
+      <Task
+        key={task.id}
+        task={task}
+        deleteTask={deleteTask}
+      />
+    ))}
+    </>
+  )
+}
+
+export default Tasks
